593d412fca4610500505c4a97bdb5b3784657e0f
Azri Farid Shahroom <azrifared8482@gmail.com>
2022-01-02 12:31:44 +0800

    added map

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
 import type { AppProps } from 'next/app';
+import 'maplibre-gl/dist/maplibre-gl.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+type Props = AppProps & {
+  mapToken: string;
+};
+
+const App = ({ Component, pageProps, mapToken }: Props) => (
+  <Component {...pageProps} mapToken={mapToken} />
+);
+
+App.getInitialProps = () => {
+  return { mapToken: process.env.MAP_TOKEN }
 }
 
-export default MyApp
+export default App
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,48 @@
+import { useState } from 'react';
+import ReactMapGL, { GeolocateControl } from 'react-map-gl';
 
-export default function Home() {
-  return (
-   <h1>Test Home Page</h1>
-  )
+type ViewPort = {
+  latitude: number;
+  longitude: number;
+  zoom: number;
+};
+
+const geolocateControlStyle = {
+  right: 10,
+  top: 10
+};
+
+const mapGLDefault = {
+  latitude: 37.7577,
+  longitude: -122.4376,
+  zoom: 17
 }
+
+const Home = ({ mapToken }: { mapToken: string }) => {
+  const [viewport, setViewport] = useState<ViewPort>(mapGLDefault);
+
+  return (
+    <>
+      <div>Raskdakd[dak</div>
+      <ReactMapGL
+        {...viewport}
+        width="100vw"
+        height="100vh"
+        mapboxApiAccessToken={mapToken}
+        onViewportChange={(nextViewport: ViewPort) => setViewport(nextViewport)}
+      >
+        <GeolocateControl
+          style={geolocateControlStyle}
+          positionOptions={{ enableHighAccuracy: true }}
+          trackUserLocation={true}
+          auto
+        />
+      </ReactMapGL>
+       
+    </>
+  );
+  
+};
+
+export default Home;
+
